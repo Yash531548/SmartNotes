1958e05697b5d9fd996b86da8fade459a2337513
Yash531548 <yashsharma531548@gmail.com>
2025-05-15 23:40:38 +0530

    Bug React-quill

--- src/components/AddNoteForm.jsx
+++ src/components/AddNoteForm.jsx
@@ -3,9 +3,9 @@ import { addNoteSubmit } from '@/app/actions/noteAction';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import dynamic from 'next/dynamic';
+import 'react-quill-new/dist/quill.snow.css';
 
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
-import 'react-quill-new/dist/quill.snow.css';
 import toast from 'react-hot-toast';
 
 const AddNoteForm = () => {
@@ -15,7 +15,7 @@ const AddNoteForm = () => {
     const handleSubmitForm = async (event) => {
         event.preventDefault();
         try {
-           
+
             const formData = new FormData(event.currentTarget);
             formData.set('noteContent', noteContent); //  ✅ Make sure it gets into formData
             const AddNotemessage = toast.loading("Adding the Note");
@@ -35,7 +35,7 @@ const AddNoteForm = () => {
         }
     }
     return (
-        
+
         <form className='w-7/12' onSubmit={handleSubmitForm}>
             <div className='mb-4'>
                 <label htmlFor="title" className='block  text-gray-700 font-bold mb-1 text-lg'> Title</label>
@@ -44,7 +44,7 @@ const AddNoteForm = () => {
             </div>
             <div className='mb-4'>
                 <label htmlFor="noteContent" className='text-gray-700 font-bold mb-1 text-lg block'>Text Area</label>
-               
+
 
                 <ReactQuill
                     value={noteContent}
